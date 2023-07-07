b680f60e9a6bd6e57ec1175d6a6c696cd6784684
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-07-07 14:36:01 -0700

    Update contact_management_system.cpp
    Implemented an add functionality to the contact management system.

--- contact_management_system.cpp
+++ contact_management_system.cpp
@@ -12,8 +12,33 @@ class Contact
         setFirstName(std::string firstName){ this->firstName = firstName; }
         setLastName(std::string lastName){ this->lastName = lastName; }
 
+        void addContact(std::vector<Contact>&);
     private:
         std::string firstName;
         std::string lastName;
         std::string email;
+}
+
+int main()
+{
+    
+}
+
+void addContact(std::vector<Contact>& contacts)
+{
+    std::string firstName;
+    std::cout << "Enter your first name: ";
+    getline(std::cin, firstName);
+
+    std::string lastName;
+    std::cout << "Enter your last name: ";
+    getline(std::cin, lastName);
+
+    std::string email;
+    std::cout << "Enter your email: ";
+    getline(std::cin, email);
+
+    contacts.push_back(Student(firstName, lastName, email));
+
+    std::cout << firstName << ", " << lastname << ", " << email << ", " << "was successfully added." << std::endl;
 }
